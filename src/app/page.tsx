d66f794520aab784468a3c1ec4ee53bf1874ca69
Manish Patil <155523680+manishpatil55@users.noreply.github.com>
2025-01-29 03:26:19 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { Header } from "@/sections/Header";
 import{ HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
+import { TapeSection } from "@/sections/Tape";
+import { TestimonialsSection } from "@/sections/Testimonials";
+import { AboutSection } from "@/sections/About";
 
 export default function Home() {
   return (
@@ -8,6 +11,9 @@ export default function Home() {
       <Header />
       <HeroSection  />
       <ProjectsSection />
+      <TapeSection />
+      <TestimonialsSection />
+      <AboutSection />
     </div>
   );
 }
