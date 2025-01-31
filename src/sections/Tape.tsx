4ad2ef2e8de3a21e85354403bb7b75b2c724de66
Manish Patil <155523680+manishpatil55@users.noreply.github.com>
2025-02-01 00:22:49 +0530

    update

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -2,8 +2,12 @@ import { Fragment } from 'react';
 import StarIcon from '../assets/icons/star.svg';
 
 const words = [
-  "Hello",
-  "World!",
+  "Hello World!",
+  "🌏",
+  "Code",
+  "Create",
+  "Conquer",
+  "Hello World!",
   "🌏",
   "Code",
   "Create",
@@ -13,9 +17,9 @@ export const TapeSection = () => {
   return (
     <div className="py-16 lg:py-24 overflow-x-clip">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
-        <div className="flex [mask-image:image-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:15s]" >
-            {[...new Array(2)].fill(0).map((_, idx) => (
+        <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:1000s]" >
+            {[...new Array(100)].fill(0).map((_, idx) => (
               <Fragment key={idx}>
                 {words.map(word => (
                   <div key={word} className="inline-flex gap-4 items-center">
