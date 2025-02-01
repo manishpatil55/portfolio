338168c062be595776346d9a5659778ff26098a8
Manish Patil <155523680+manishpatil55@users.noreply.github.com>
2025-02-02 03:54:18 +0530

    Update

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -18,7 +18,7 @@ export const TapeSection = () => {
     <div className="py-16 lg:py-24 overflow-x-clip">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
         <div className="flex [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:1000s] max-w-xs md:max-w-md" >
+          <div className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:1000s]" >
             {[...new Array(100)].fill(0).map((_, idx) => (
               <Fragment key={idx}>
                 {words.map(word => (
