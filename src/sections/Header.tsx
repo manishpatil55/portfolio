4921f98139bd39bf846ad6e982e3cc267730f608
Manish Patil <155523680+manishpatil55@users.noreply.github.com>
2025-02-02 04:06:01 +0530

    Update

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,16 +2,16 @@ export const Header = () => {
   return (
       <div className="flex justify-center items-center fixed top-3 w-full z-10">
           <nav className="flex gap-1 p-0.5 border-white/15 rounded-full bg-white/10 backdrop-blur">
-              <a href="#home" className="nav-item">
+              <a href="home" className="nav-item">
                   Home
               </a>
-              <a href="#projects" className="nav-item">
+              <a href="projects" className="nav-item">
                   Projects
               </a>
-              <a href="#about" className="nav-item">
+              <a href="about" className="nav-item">
                   About
               </a>
-              <a href="#contact" className="nav-item bg-white text-gray-950 hover:bg-white/75 hover:text-gray-950">
+              <a href="contact" className="nav-item bg-white text-gray-950 hover:bg-white/75 hover:text-gray-950">
                   Contact
               </a>
           </nav>
