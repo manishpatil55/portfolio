79a4085d274ffb6988e6206587ceab0fcc637317
Manish Patil <155523680+manishpatil55@users.noreply.github.com>
2025-02-02 05:34:31 +0530

    Update

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -21,22 +21,16 @@ const footerLinks = [
 
 export const Footer = () => {
   return (
-    <footer id="footer" className="relative -z-10 overflow-x-clip">
-      <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10"></div>
-      
+    <footer id="footer" className='relative -z-10 overflow-x-clip'>
+      <div className='absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10'></div>
       <div className="container">
-        <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
-          <div className="text-white/40">&copy; 2025. All rights reserved.</div>
+        <div className='border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8'>
+          <div className='text-white/40'>&copy; 2025. All rights reserved.</div>
           <div>
-            <nav className="flex flex-col md:flex-row items-center gap-8 z-20 ">
+            <nav className='flex flex-col md:flex-row items-center gap-8'>
               {footerLinks.map(link => (
-                <a
-                  href={link.href}
-                  key={link.title}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <span className="font-semibold">{link.title}</span>
+                <a href={link.href} key={link.title} className='inline-flex items-center gap-1.5'>
+                  <span className='font-semibold'>{link.title}</span>
                   <ArrowUpRightIcon className="size-4" />
                 </a>
               ))}
@@ -46,4 +40,4 @@ export const Footer = () => {
       </div>
     </footer>
   );
-};           
+};
